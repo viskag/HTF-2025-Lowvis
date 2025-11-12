4aaffb88986f3bd9727eb196d49449421d677bb2
viskag <s144510@ap.be>
2025-11-12 14:16:09 +0100

    Add project files.

---
+++ GameNavigator.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTF
+{
+    public static class GameNavigator
+    {
+        public static void NavigateToUrl(IWebDriver driver, string url)
+        {
+            driver.Navigate().GoToUrl(url);
+            Console.WriteLine($"Navigated to: {url}");
+        }
+
+        public static void WaitAndClick(IWebDriver driver, By locator, int timeoutSeconds = 10)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSeconds));
+            var element = wait.Until(d => d.FindElement(locator));
+            element.Click();
+        }
+
+        public static IWebElement WaitForElement(IWebDriver driver, By locator, int timeoutSeconds = 10)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSeconds));
+            return wait.Until(d => d.FindElement(locator));
+        }
+    }
+}
